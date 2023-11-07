198b84256a31227127a61a1e0feb154d82793693
Victor Henrique Fernandes <victorhenriquefernandes2@gmail.com>
2023-11-07 13:26:00 -0300

    tirando consolelog

--- backend/src/middlewares/validateUpdateSales.js
+++ backend/src/middlewares/validateUpdateSales.js
@@ -7,7 +7,6 @@ const validateUpdateSales = async (req, res, next) => {
   const salesIdExist = await salesModel.findById(id);
   if (!salesIdExist.length) return res.status(404).json({ message: 'Sale not found' });
   const productsId = salesIdExist.map((product) => product.productId);
-  console.log(productsId);
   const productsIncludes = productsId.includes(Number(productId));
   if (!productsIncludes) {
     return res.status(404).json({ message: 'Product not found in sale' }); 
